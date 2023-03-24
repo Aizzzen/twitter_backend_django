73b557c33a2af7f9d717456629ff846d9e028f69
Yunus <gadamurrr@gmail.com>
2023-03-24 07:53:32 +0300

    tweet_viewset

---
+++ backend/tweet/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator
+from django.db import models
+
+
+class Tweet(models.Model):
+    text = models.TextField(validators=[MaxLengthValidator(280)])
+    is_created = models.DateTimeField(auto_now_add=True)
+    is_updated = models.DateTimeField(auto_now=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # likes = models.PositiveIntegerField(null=True, default=0)
+    # retweets
+    # replies
