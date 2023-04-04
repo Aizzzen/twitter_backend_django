d3a4bfef6e440370506afec03d34cdd88fb2235e
Yunus <gadamurrr@gmail.com>
2023-04-05 01:51:42 +0300

    only usable

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=255, null=True)
-    fullname = models.CharField(max_length=255, null=True)
-    about = models.CharField(max_length=255, null=True)
-    website = models.CharField(max_length=255, null=True)
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     location = models.CharField(max_length=255, null=True)
+#     fullname = models.CharField(max_length=255, null=True)
+#     about = models.CharField(max_length=255, null=True)
+#     website = models.CharField(max_length=255, null=True)
--- backend/user/views.py
+++ backend/user/views.py
@@ -9,19 +9,19 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from tweet.models import Tweet
-from .serializers import UserSerializer, UserProfileSerializer
+from .serializers import UserSerializer     # UserProfileSerializer
 
 
-@api_view(['GET'])
-def get_all_user_data(request: Request):
-    return Response({
-        # 'data': {
-        **UserSerializer(request.user).data,
-        **UserProfileSerializer(request.user).data,
-        # 'tweets': Tweet.objects.all().values()  # - если не авторизован
-        'tweets': Tweet.objects.filter(user=request.user).values()  # - если авторизован
-        # },
-    })
+# @api_view(['GET'])
+# def get_all_user_data(request: Request):
+#     return Response({
+#         # 'data': {
+#         **UserSerializer(request.user).data,
+#         # **UserProfileSerializer(request.user).data,
+#         # 'tweets': Tweet.objects.all().values()  # - если не авторизован
+#         'tweets': Tweet.objects.filter(user=request.user).values()  # - если авторизован
+#         # },
+#     })
 
 
 class ActivationView(View):
