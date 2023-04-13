2de109efb8203ef8db55654f73986feb378d9b3e
Yunus <gadamurrr@gmail.com>
2023-04-13 16:21:55 +0300

    added actions

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 
-from user.views import ActivationView, get_user_data, UsersListAPIView
+from user.views import ActivationView, get_user_data
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('activate/<uidb64>/<token>/', ActivationView.as_view(), name='activate'),
     path('user-data/', get_user_data),
-    path('users-list/', UsersListAPIView.as_view()),
 ]
